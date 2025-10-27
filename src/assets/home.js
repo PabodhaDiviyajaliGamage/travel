6a82b8f46ac11878b763685cb4659bba32e34c5c
Pabodha Gamage <75441928+PabodhaDiviyajaliGamage@users.noreply.github.com>
2025-10-27 14:06:27 +0530

    Add files via upload

---
+++ src/assets/home.js
@@ -0,0 +1,40 @@
+import home1 from "./home1.jpg";
+import home2 from "./home2.jpg";
+import home3 from "./home3.jpg";
+import home4 from "./home4.jpg";
+import home5 from "./home5.jpg";
+
+
+
+
+const homepageImages = [
+    {
+         title: "Nade Villa And Tourism",
+    subtitle: "Start your journey with us",
+    background:home1,
+    },
+    {
+        title: "Nade Villa And Tourism",
+    subtitle: "Start your journey with us",
+    background:home2,
+    },
+    {
+          title: "Nade Villa And Tourism",
+    subtitle: "Start your journey with us",
+    background:home3,
+    },
+    {
+          title: "Nade Villa And Tourism",
+    subtitle: "Start your journey with us",
+    background:home4,
+    },
+    {
+          title: "Nade Villa And Tourism",
+    subtitle: "Start your journey with us",
+    background:home5,
+    },
+  
+
+];
+
+export default homepageImages;
